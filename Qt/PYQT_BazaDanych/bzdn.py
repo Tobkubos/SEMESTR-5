39efa63e4414e26683eb61aee7cf7423faf4e252
Tobkubos <128313959+Tobkubos@users.noreply.github.com>
2024-12-19 18:25:11 +0100

    .

--- Qt/PYQT_BazaDanych/bzdn.py
+++ Qt/PYQT_BazaDanych/bzdn.py
@@ -1,7 +1,8 @@
 import sys
 from PyQt6 import QtSql
+from PyQt6.QtCore import Qt
 from PyQt6.QtSql import QSqlQuery, QSqlDatabase 
-from PyQt6.QtWidgets import QApplication, QWidget, QTableView, QHBoxLayout
+from PyQt6.QtWidgets import QApplication, QWidget, QTableView, QHBoxLayout, QVBoxLayout, QLabel, QPushButton, QComboBox, QLineEdit, QAbstractItemView
 
 def add_person(name, surname):
     query = QSqlQuery()
@@ -9,7 +10,20 @@ def add_person(name, surname):
     query.addBindValue(name)
     query.addBindValue(surname)
     if not query.exec():
-        print("Nie udalo sie dodac osoby:", query.lastError().text())
+        infoLabel.setText("Nie udalo sie dodac osoby:", query.lastError().text())
+    else:
+        infoLabel.setText("DODANO OSOBĘ!")
+        
+def edit_person(OsID, NewName, NewSurname):
+    query = QSqlQuery()
+    query.prepare("UPDATE osoby SET name = ?, surname = ? WHERE PersonID = ?")
+    query.addBindValue(NewName)
+    query.addBindValue(NewSurname)
+    query.addBindValue(OsID)
+    if not query.exec():
+        infoLabel.setText("Nie udalo sie dodac osoby:", query.lastError().text())
+    else:
+        infoLabel.setText("USUNIĘTO OSOBĘ!")
         
 def add_book(title, author):
     query = QSqlQuery()
@@ -17,7 +31,9 @@ def add_book(title, author):
     query.addBindValue(title)
     query.addBindValue(author)
     if not query.exec():
-        print("Nie udalo sie dodac ksiazki:", query.lastError().text())
+        infoLabel.setText("Nie udalo sie dodac ksiazki:", query.lastError().text())
+    else:
+        infoLabel.setText("DODANO KSIĄŻKĘ!")
 
 def add_wypozycznia(osID, ksiazkaID):
     query = QSqlQuery()
@@ -25,7 +41,18 @@ def add_wypozycznia(osID, ksiazkaID):
     query.addBindValue(osID)
     query.addBindValue(ksiazkaID)
     if not query.exec():
-        print("Nie udalo sie dodac ksiazki:", query.lastError().text())
+        infoLabel.setText("Nie udalo sie dodac wypozyczneia:", query.lastError().text())
+    else:
+        infoLabel.setText("DODANO WYPOŻYCZENIE!")
+
+def del_wypozycznia(wypID):
+    query = QSqlQuery()
+    query.prepare("DELETE FROM wypozyczenia WHERE LoanID = ?")
+    query.addBindValue(wypID)
+    if not query.exec():
+        infoLabel.setText("Nie udalo sie usunac wypozyczenia:", query.lastError().text())
+    else:
+        infoLabel.setText("USUNUĘTO WYPOŻYCZENIE!")
 
 
 def create_table_osoby():
@@ -40,7 +67,7 @@ def create_table_osoby():
     )
     """
     if not query.exec(create_table_sql):
-        print("Nie udalo sie stworzyc tabeli:", query.lastError().text())
+        infoLabel.setText("Nie udalo sie stworzyc tabeli:", query.lastError().text())
     
 def create_table_ksiazki():
     query = QSqlQuery()
@@ -54,7 +81,7 @@ def create_table_ksiazki():
     )
     """
     if not query.exec(create_table_sql):
-        print("Nie udalo sie stworzyc tabeli:", query.lastError().text())
+        infoLabel.setText("Nie udalo sie stworzyc tabeli:", query.lastError().text())
 
 def create_table_wypozyczenia():
     query = QSqlQuery()
@@ -70,7 +97,7 @@ def create_table_wypozyczenia():
     )
     """
     if not query.exec(create_table_sql):
-        print("Nie udało się stworzyć tabeli wypożyczeń:", query.lastError().text())
+        infoLabel.setText("Nie udało się stworzyć tabeli wypożyczeń:", query.lastError().text())
         
 app = QApplication([])
 
@@ -82,12 +109,209 @@ db = QSqlDatabase.addDatabase("QSQLITE")
 db.setDatabaseName("book_borrow.db")
 
 if not db.open():
-    print("Polaczenie nie udane:", db.lastError().text())
     sys.exit(1)
 
 create_table_osoby()
 create_table_ksiazki()
 create_table_wypozyczenia()
+
+#osoby
+model = QtSql.QSqlTableModel()
+model.setTable("osoby")
+model.select()
+
+view = QTableView()
+view.setEditTriggers(QAbstractItemView.EditTrigger.NoEditTriggers)
+view.setModel(model)
+#
+
+#ksiazki
+model2 = QtSql.QSqlTableModel()
+model2.setTable("ksiazki")
+model2.select()
+
+view2 = QTableView()
+view2.setEditTriggers(QAbstractItemView.EditTrigger.NoEditTriggers)
+view2.setModel(model2)
+#
+
+#wypozyczenia
+model3 = QtSql.QSqlQueryModel()
+query3 = """
+    SELECT w.LoanID, o.Name, o.Surname, k.Title, k.Author
+    FROM wypozyczenia w
+    JOIN osoby o ON w.PersonID = o.PersonID
+    JOIN ksiazki k ON w.BookID = k.BookID
+"""
+model3.setQuery(query3)
+
+view3 = QTableView()
+view3.setModel(model3)
+view3.setEditTriggers(QAbstractItemView.EditTrigger.NoEditTriggers)
+view3.show()
+
+#
+layout = QHBoxLayout()
+layout.addWidget(view)
+layout.addWidget(view2)
+layout.addWidget(view3)
+
+
+mainLayout = QVBoxLayout()
+mainLayout.addLayout(layout)
+
+Sections = QHBoxLayout()
+
+#region DODAJ OSOBĘ
+AddPersonLayout = QVBoxLayout()
+imieLabel = QLabel("Imie")
+imieLabel.setFixedHeight(30)
+imie = QLineEdit()
+imie.setFixedHeight(30)
+AddPersonLayout.addWidget(imieLabel)
+AddPersonLayout.addWidget(imie)
+
+NazwiskoLabel = QLabel("Nazwisko")
+NazwiskoLabel.setFixedHeight(30)
+nazwisko = QLineEdit()
+nazwisko.setFixedHeight(30)
+AddPersonLayout.addWidget(NazwiskoLabel)
+AddPersonLayout.addWidget(nazwisko)
+AddPerson = QPushButton("DODAJ OSOBĘ")
+def F_addPerson():
+    if imie.text().strip() != "" and nazwisko.text().strip() != "":
+        print(imie.text().strip(), nazwisko.text().strip())
+        add_person(imie.text().strip(), nazwisko.text().strip())
+        model.select()
+    else:
+        infoLabel.setText("NIE WPROWADZONO DANYCH!")
+        
+AddPerson.clicked.connect(F_addPerson)
+AddPersonLayout.addWidget(AddPerson)
+
+Sections.addLayout(AddPersonLayout,1)
+#endregion
+#region DODAJ KSIĄŻKĘ
+AddBookLayout = QVBoxLayout()
+TytulLabel = QLabel("Tytul")
+TytulLabel.setFixedHeight(30)
+tytul = QLineEdit()
+tytul.setFixedHeight(30)
+AddBookLayout.addWidget(TytulLabel)
+AddBookLayout.addWidget(tytul)
+
+AutorLabel = QLabel("Autor")
+AutorLabel.setFixedHeight(30)
+autor = QLineEdit()
+autor.setFixedHeight(30)
+AddBookLayout.addWidget(AutorLabel)
+AddBookLayout.addWidget(autor)
+
+AddBook = QPushButton("DODAJ KSIĄŻKĘ")
+def F_addBook():
+    if tytul.text().strip() != "" and autor.text().strip() != "":
+        print(tytul.text().strip(), autor.text().strip())
+        add_book(tytul.text().strip(), autor.text().strip())
+        model2.select()
+    else:
+        infoLabel.setText("NIE WPROWADZONO DANYCH!")
+        
+AddBook.clicked.connect(F_addBook)
+AddBookLayout.addWidget(AddBook)
+
+Sections.addLayout(AddBookLayout,1)
+#endregion
+#region OPCJE - WYPOŻYCZ / USUŃ / POSORTUJ
+Options = QVBoxLayout()
+wypozycz = QPushButton("wypożycz")
+
+def F_wypozycz():
+    indexOsoby = view.selectionModel().currentIndex()
+    indexKsiazki = view2.selectionModel().currentIndex()
+    if indexOsoby.isValid() and indexKsiazki.isValid():
+        personID = model.record(indexOsoby.row()).value("PersonID")
+        bookID = model2.record(indexKsiazki.row()).value("BookID")
+        
+        add_wypozycznia(personID, bookID)
+        model3.setQuery(query3)
+        
+    else:
+        infoLabel.setText("NIE ZAZNACZONO OSOBY LUB KSIĄŻKI!")
+
+wypozycz.clicked.connect(F_wypozycz)
+wypozycz.setFixedHeight(30)
+Options.addWidget(wypozycz)
+
+delWyp = QPushButton("Usuń Wypożyczenie")
+
+def F_delWyp():
+    indexWyp = view3.selectionModel().currentIndex()
+    if indexWyp.isValid():
+        wypID = model3.record(indexWyp.row()).value("LoanID")
+        
+        del_wypozycznia(wypID)
+        model3.setQuery(query3)
+        
+    else:
+        infoLabel.setText("NIE ZAZNACZONO WYPOŻYCZENIA!")
+
+delWyp.clicked.connect(F_delWyp)
+delWyp.setFixedHeight(30)
+Options.addWidget(delWyp)
+
+sort = QComboBox()
+sort.addItem("Sortuj wg Imienia (osoby)")
+sort.addItem("Sortuj wg Nazwiska (osoby)")
+sort.addItem("Sortuj wg Tytułu (książki)")
+sort.addItem("Sortuj wg Autora (książki)")
+sort.addItem("Sortuj wg Wypożyczenia (osoby)")
+sort.addItem("Sortuj wg Wypożyczenia (książki)")
+
+def F_sort(index):
+    if index == 0:  # Sortuj po imieniu
+        model.setSort(1, Qt.SortOrder.AscendingOrder)
+        model.select()
+    elif index == 1:  # Sortuj po nazwisku
+        model.setSort(2, Qt.SortOrder.AscendingOrder)
+        model.select()
+    elif index == 2:  # Sortuj po tytule
+        model2.setSort(1, Qt.SortOrder.AscendingOrder) 
+        model2.select()
+    elif index == 3:  # Sortuj po autorze
+        model2.setSort(2, Qt.SortOrder.AscendingOrder) 
+        model2.select()
+    elif index == 4:  # Sortuj po osobie wypo
+        model3.setQuery("""
+            SELECT w.LoanID, o.Name, o.Surname, k.Title, k.Author
+            FROM wypozyczenia w
+            JOIN osoby o ON w.PersonID = o.PersonID
+            JOIN ksiazki k ON w.BookID = k.BookID
+            ORDER BY o.Name ASC
+        """)
+    elif index == 5:  # Sortuj po ksiazce wypo
+        model3.setQuery("""
+            SELECT w.LoanID, o.Name, o.Surname, k.Title, k.Author
+            FROM wypozyczenia w
+            JOIN osoby o ON w.PersonID = o.PersonID
+            JOIN ksiazki k ON w.BookID = k.BookID
+            ORDER BY k.Title ASC
+        """)
+
+sort.currentIndexChanged.connect(F_sort)
+sort.setFixedHeight(30)
+Options.addWidget(sort)
+
+Sections.addLayout(Options,1)
+#endregion
+
+info = QVBoxLayout()
+infoLabel = QLabel()
+info.addWidget(infoLabel)
+
+mainLayout.addLayout(Sections)
+mainLayout.addLayout(info)
+window.setLayout(mainLayout)
+
 add_person("John", "Doe")
 add_person("Jane", "Smith")
 
@@ -97,45 +321,9 @@ add_book("bandyta", "remi mroz")
 add_wypozycznia(1,1)
 add_wypozycznia(2,1)
 
-
-#
-model = QtSql.QSqlTableModel()
-model.setTable("osoby")
-model.select()
-
-view = QTableView()
-view.setModel(model)
-#
-
-#
-model2 = QtSql.QSqlTableModel()
-model2.setTable("ksiazki")
 model2.select()
-
-view2 = QTableView()
-view2.setModel(model2)
-#
-
-#
-model3 = QtSql.QSqlQueryModel()
-query = """
-    SELECT o.Name, o.Surname, k.Title, k.Author
-    FROM wypozyczenia w
-    JOIN osoby o ON w.PersonID = o.PersonID
-    JOIN ksiazki k ON w.BookID = k.BookID
-"""
-model3.setQuery(query)
-
-view3 = QTableView()
-view3.setModel(model3)
-view3.show()
-#
-
-layout = QHBoxLayout()
-layout.addWidget(view)
-layout.addWidget(view2)
-layout.addWidget(view3)
-window.setLayout(layout)
+model.select()
+model3.setQuery(query3)
 
 window.show()
 
